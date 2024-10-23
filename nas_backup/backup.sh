c288f4e804774cf07d024f09adc82ee3a2958ea5
cl-ire <s85609@htw-dresden.de>
2024-10-23 13:59:30 +0200

    fix

--- nas_backup/backup.sh
+++ nas_backup/backup.sh
@@ -16,21 +16,9 @@ backup_to_nas=true
 # Set backup variables
 set_variables() {
     current_date_time=$(date +"%d-%m-%Y_%H-%M")
-    
-    log_file="/var/log/backup.log"
-
     home_server_path="/home/home_server"
     nas_path="/home/nas"
     backup_path="$nas_path/docker-backup-$current_date_time"
-}
-
-# set backup variables
-set_variables () {
-    current_date_time=$(date +"%d-%m-%Y_%H-%M")
-    file_name="$repository_name-backup-$current_date_time"
-    path="/home/github_nas_backup/backup"
-    backup_path="$path/$file_name"
-    
 }
 
 # Backup function
@@ -44,10 +32,8 @@ backup() {
     echo "Backing up project directory..."
     cp -r $home_server_path $backup_path || { echo "Failed to backup home server to nas"; exit 1; }
 
-
     # Restart containers
     # docker-compose start vaultwarden wg-easy pihole yacht
-
     echo "Backup completed successfully."
 }
 
